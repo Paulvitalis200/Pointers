a6416e1c5cecee0723eb8a7988a2ddb1c2a3935f
Paul Otieno <vitalispaul48@live.com>
2025-11-29 17:27:44 +0300

    Dynamic memory allocation

--- main.cpp
+++ main.cpp
@@ -2,16 +2,24 @@
 
 
 int main() {
-    int x = 10;
-    int y = 20;
+    // Dynamic memory allocation
 
-    int* ptrX = &x;
-    int* ptrY = &x;
+    // We can declare an array and set it to have e.g 10 items
+    // However, its fixed and if it grows, What to do?
+    // Also if we receive only 1 item in the array. That is wasted memory
+    // This is where dynamic memory allocation comes in
 
-    if (ptrX != nullptr)
-        std::cout << *ptrX;
+    // Saved in stack memory
+    // int numbers[10];
 
-    // We can compare ptr addresses directly and see if they match or if they point to a nullptr
-    // We can also dereference the values and compare them
+    // Heap (Free store) - Stored here, We are responsible for the cleanup.
+    int* numbers = new int[10]; // Initialize an initial size
+    int* number = new int; // Technically we never want to do this. But we';; see later
+    delete number;
+    delete[] numbers; // Deallocate the memory
+    number = nullptr;
+    numbers = nullptr; // Not mandatory but good practice
+
+
     return 0;
 }
