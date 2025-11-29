3d739279bdacbc8c029ea493bc16417d9b19f820
Paul Otieno <vitalispaul48@live.com>
2025-11-29 15:06:13 +0300

    Passing pointers to functions

--- main.cpp
+++ main.cpp
@@ -1,40 +1,25 @@
 #include <iostream>
 
+
+void increasePrice(double* price) {
+    *price *= 2;
+}
+
 int main() {
 
-    // Pointers & Constants
+    // Passing pointers to functions
 
-    // CASE 1: Data is constant
-    const int x = 10;
-    const int* ptr = &x;
+    // Modern way of passing large stuff to functions is using references
+    // We can also solve this using pointers though. It will have a little
+    // more work though
 
-    int y = 20;
-    ptr = &y;
+    // We pass the address as the parameters to the increasePrice function
+    // We then declare the pointer as the required parameter using double* price
+    // Lastly we dereference the pointer to get the value of the price. *price *= 2
 
-
-    // If x is a constant, our pointer should also be a const int pointer e.g const int x; const int* ptr
-    // We cannot set the value of the *ptr to another value.
-    // We can however point the pointer to the address of another variable e.g y. Our pointer is not constant
-
-    // CASE 2: Pointer is constant
-    int w = 10;
-    int* const newptr = &w;
-
-    // To define a constant pointer, we add const after the pointer type declaration
-    // e.g int* const newptr = &w;
-
-    int q = 30;
-    newptr = &q; // This won't work because our pointer is constant
-
-    // When declaring a constant pointer, we should always initialize it. If we don't, later on we cannot
-    // change the variable
-
-
-    // CASE 3: Both data and pointer are constant
-    const int a = 20;
-    const int* const bptr = &a;
-
-    // We have a constant integer, pointing to a constant pointer
+    double price = 100;
+    increasePrice(&price);
+    std::cout << price;
 
     return 0;
 }
