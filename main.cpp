7eafbde920de35ae11cc026f7725f9a174f2fb8c
Paul Otieno <vitalispaul48@live.com>
2025-11-29 17:59:28 +0300

    Smart pointer description

--- main.cpp
+++ main.cpp
@@ -2,38 +2,14 @@
 
 
 int main() {
-    // Dynamic resizing an array
 
-    int capacity = 5;
-    int* numbers = new int[capacity];
-    int entries = 0;
+    int* x = new int;
+    delete x;
+    delete x;
 
-    while (true) {
-        std::cout << "Number: ";
-        std::cin >> numbers[entries];
-
-        if (std::cin.fail()) break;
-        entries++;
-
-        if (entries == capacity) {
-            capacity = capacity * 2;
-            // Create a temp array (twice the size)
-            int* tempNumbers = new int[capacity];
-            // Copy all the elements
-            for (int i = 0; i < entries; i++)
-                tempNumbers[i] = numbers[i];
-            // Have "numbers" pointer point to the new array
-            delete[] numbers; // delete initial memory before
-            numbers = tempNumbers;
-        }
-    }
-
-    for (int i = 0; i < entries; i++)
-        std::cout << numbers[i] << std::endl;
-
-    delete[] numbers;
+     // If we delete pointers twice. We'll get an error. This is why we have smart pointers
+    // to free us from the hassle. Managing pointers can be hard
 
     return 0;
 }
 
-// WE NEVER HAVE TO DO THIS. We have smart pointers
